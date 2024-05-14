07660d4636124bec2a47c70db8e8ab833fc0c6b8
kotansava <ansava0@yandex.ru>
2024-05-14 19:33:10 +0400

    Исправлен ряд чисел Фибоначи

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,12 @@
 print("Hello, world!")
 
 def fib(n):
-    if n in (1, 2):
+    if n-1 <= 0:
+        return 0
+    elif n-1 in (1, 2):
         return 1
     return fib(n - 1) + fib(n - 2)
 
 
 print(fib(10))
+
