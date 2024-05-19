23dbbb8c682b1ef902deda5fe174f9a24379bf3c
kotansava <ansava0@yandex.ru>
2024-05-19 20:32:36 +0400

    Исправлен итеративный вывод

--- helloworld.py
+++ helloworld.py
@@ -1,15 +1,13 @@
 print("Hello, world!")
 
 def fib(n):
-    if n-1 <= 0:
-        return 0
-    elif n-1 in (1, 2):
-        return 1
-    return fib(n - 1) + fib(n - 2)
+    seq = [0, 1]
+    for i in range(2, n):
+        nn = seq[-1] + seq[-2]
+        seq.append(nn)
 
-def cycle_fib(n):
-    for i in range(n+1):
-        print(fib(i))
+    for i in seq:
+        print(i)
+    
 
-
-cycle_fib(10)
+fib(10)
